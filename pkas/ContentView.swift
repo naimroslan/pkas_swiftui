10f41065c583971ec89cf34c97358c1391db3662
naimroslan <heyitsnaim@gmail.com>
2021-07-10 12:19:45 +0800

    Initial Commit

---
+++ pkas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pkas
+//
+//  Created by Naim Roslan on 10/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pkas/pkasApp.swift
@@ -0,0 +1,17 @@
+//
+//  pkasApp.swift
+//  pkas
+//
+//  Created by Naim Roslan on 10/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct pkasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
